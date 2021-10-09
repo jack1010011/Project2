2bf2a120d8b77697d65cc1143fd3e8b7dcbcdce4
HÉCTOR FERNÁNDEZ MÉNDEZ <hecferme@gmail.com>
2021-10-09 17:27:21 -0600

    Consultas LinQ

--- Topicos.NetCore.ConsoleApp/LogicaPrincipal.cs
+++ Topicos.NetCore.ConsoleApp/LogicaPrincipal.cs
@@ -16,10 +16,51 @@ namespace Topicos.NetCore.ConsoleApp
 
         public void HagaLaMagia()
         {
+            ConsultaPorNombreAproximado();
+            //ConsultaPorPaisAproximado();
+        }
+
+        private void ConsultaPorNombreAproximado()
+        {
+            var elNombreAproximado = "as";
+            var elServicio = new Topicos.Netcore.NorthWnd.BL.Logica.AccesoBd.Customer();
+            var elResultado = elServicio.BuscarPorNombreAproximado(elNombreAproximado);
+            ImprimirCustomers(elResultado);
+        }
+
+        private void ConsultaPorPaisAproximado()
+        {
+            var elPaisAproximado = "ana";
+            var elServicio = new Topicos.Netcore.NorthWnd.BL.Logica.AccesoBd.Customer();
+            var elResultado = elServicio.BuscarPorNombreAproximadoDelPais(elPaisAproximado);
+            ImprimirCustomers(elResultado);
+        }
+
+        private void ImprimirCustomers(IList<Netcore.NorthWnd.Model.MyModels.Customer> elResultado)
+        {
+            if (elResultado == null)
+            {
+                System.Console.WriteLine("Lista sin elementos");
+                return;
+            }
+            foreach (var customer in elResultado)
+            {
+                System.Console.WriteLine($"ID: {customer.CustomerId} - Nombre Completo: {customer.FullName} - Telefono: {customer.Phone}");
+                System.Console.WriteLine("Direcciones:");
+                foreach (var direccion in customer.CustomerAddresses)
+                {
+                    System.Console.WriteLine($"   - Address Line 1: {direccion.Address.AddressLine1} - City: {direccion.Address.City} - State: {direccion.Address.StateProvince} - Country: {direccion.Address.CountryRegion}");
+                }
+            }
+        }
+
+        public void HagaLaMagiaOldV2()
+        {
             var elMensaje = new Mensaje("Yo", "Usted", "Hello World!");
             var elServicio = new MensajeriaPlana();
             elServicio.Enviar(elMensaje);
         }
+
 
     }
 }
---
+++ Topicos.NetCore.ConsoleApp/Topicos.NetCore.NorthWnd.BL/Logica/AccesoBb/Customer.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topicos.Netcore.NorthWnd.BL.Logica.AccesoBd
+{
+    public class Customer
+    {
+        private static Model.MyModels.AdventureWorksLT2019Context elContexto = new Model.MyModels.AdventureWorksLT2019Context();
+
+        public Model.MyModels.Customer BuscarPorId (int customerId)
+        {
+            Model.MyModels.Customer resultado;
+            // resultado = elContexto.Customers.Find(customerId);
+            resultado = elContexto.Customers.Where(c => c.CustomerId == customerId).FirstOrDefault();
+            return resultado;
+        }
+
+        public IList<Model.MyModels.Customer> BuscarPorNombreAproximadoDelPais (string nombreAproximadoDelPais)
+        {
+            IList<Model.MyModels.Customer> resultado;
+            using (var _elContexto = new Model.MyModels.AdventureWorksLT2019Context())
+            {
+                var laConsulta = _elContexto.Customers.Include(c => c.CustomerAddresses).ThenInclude(ca => ca.Address).Where(c => c.CustomerAddresses.Any(ca => ca.Address.CountryRegion.Contains(nombreAproximadoDelPais))).OrderByDescending(c => c.Phone);
+                resultado = laConsulta.ToList();
+            }
+            return resultado;
+        }
+
+        public IList<Model.MyModels.Customer> BuscarPorNombreAproximadoConErrores(string nombreAproximado)
+        {
+            IList<Model.MyModels.Customer> resultado;
+            using (var _elContexto = new Model.MyModels.AdventureWorksLT2019Context())
+            {
+                resultado = _elContexto.Customers.Where(c => c.FullName.Contains(nombreAproximado)).OrderByDescending(c => c.Phone).ToList();
+            }
+            return resultado;
+        }
+
+        public IList<Model.MyModels.Customer> BuscarPorNombreAproximado(string nombreAproximado)
+        {
+            IList<Model.MyModels.Customer> temporal;
+            using (var _elContexto = new Model.MyModels.AdventureWorksLT2019Context())
+            {
+                temporal = _elContexto.Customers.OrderByDescending(c => c.Phone).ToList().Where(c => c.FullName.Contains(nombreAproximado)).ToList();
+            }
+            var resultado = temporal;
+            return resultado;
+        }
+
+
+    }
+}
