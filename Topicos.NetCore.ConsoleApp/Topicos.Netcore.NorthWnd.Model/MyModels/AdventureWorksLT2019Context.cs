7a75210debacc9f4b571fbc42d0d88c59da0278e
HÉCTOR FERNÁNDEZ MÉNDEZ <hecferme@gmail.com>
2021-11-06 15:36:53 -0600

    Se traslada la conexión con la base de datos al archivo appsettins.json, para que no se incluya en los commits.

--- Topicos.NetCore.ConsoleApp/Topicos.Netcore.NorthWnd.Model/MyModels/AdventureWorksLT2019Context.cs
+++ Topicos.NetCore.ConsoleApp/Topicos.Netcore.NorthWnd.Model/MyModels/AdventureWorksLT2019Context.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 #nullable disable
 
@@ -25,8 +26,11 @@ namespace Topicos.Netcore.NorthWnd.Model.MyModels
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local);Database=AdventureWorksLT2019;Trusted_Connection=True;");
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("miAzureServerlessDB"));
             }
         }
 
